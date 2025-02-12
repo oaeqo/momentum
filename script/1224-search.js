8421f8e1bfb26fb953342cc96ed195493cea9a2e
oaeqo <wjdals7898@naver.com>
2025-02-12 15:24:48 +0900

    delete

--- script/1224-search.js
+++
@@ -1,9 +0,0 @@
-
-let infoElem = document.querySelector(".search-bar > input")
-let searchBtn = document.querySelector(".search-bar > a")
-
-searchBtn.onclick = function() {
-  let url = 'https://www.google.com/search?q='+infoElem.value;
-  window.open(url);
-  infoElem.value = null;
-}
